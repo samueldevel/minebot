d2efa281b8ed4270b997d36d1961cb6001f7118b
Samuel Elias <samuel.elias.dev@gmail.com>
2022-09-19 22:15:17 -0300

    feat: applications provided by start.spring.io

---
+++ src/main/java/com/samuel/minebot/MinebotApplication.java
@@ -0,0 +1,13 @@
+package com.samuel.minebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinebotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinebotApplication.class, args);
+    }
+
+}
